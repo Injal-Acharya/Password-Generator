5a201100c77adb389b720a322513880b8b8c8ba5
AI-3Tech <injalacharya1@gmail.com>
2025-01-23 22:18:44 +0545

    Password Generator

--- GeneratePass.js
+++ GeneratePass.js
@@ -1,7 +1,7 @@
 class Password {
   constructor() {
-    this.lowercase = "abcdefghijklmnopqrstuvwxyz";
-    this.uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+    this.lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+    this.uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXY"];
     this.numbers = "0123456789";
     this.special = "~!@#$%^&*()_+-=?/*";
   }
@@ -13,6 +13,25 @@ class Password {
   }
 
   weakPassword () {
-    
+    let len = 0;
+    let max = Math.floor(Math.random() * (5) + 8);
+    let pass  = "";
+    while (len < max) {
+      let choose = Math.floor(Math.random() * 2);
+      if (choose == 0) {
+        pass += this.lowercase[Math.floor(Math.random() * 25)];
+      }
+      else {
+        pass += this.uppercase[Math.floor(Math.random() * 25)];
+      }
+      len++;
+    }
+    return pass;
   }
 }
+
+let choice;
+let newPass = new Password;
+choice = prompt("Enter choice (funny, weak):");
+if (choice == "funny") console.log(newPass.funnyPassword());
+if(choice == "weak") console.log(newPass.weakPassword());
